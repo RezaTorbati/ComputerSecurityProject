5daa399dbee1cc40cd264a53fae39b7bb2c88f24
Zachary Singleton <za.sing227@gmail.com>
2021-11-23 11:15:17 -0600

    Class to infer APRICOT images and class to crop out bboxes

--- CropBBox.py
+++ CropBBox.py
@@ -1,56 +1,47 @@
+# This class was adapted from the a combination of the crop_objects() provided in:
+# https://github.com/theAIGuysCode/yolov4-custom-functions
+# With some help from yolov4/common/base_class.py and yolov4/common/media.py
+
+import os
 import cv2
-from yolov4.tf import YOLOv4
 import numpy as np
-from pynput.keyboard import Key, Controller
 
-class CropBBox:
+# function for cropping each detection and saving as new image
+def crop_objects(img, bboxes, path):
+    class_names = {}
+    with open('modelData/coco.names', 'r') as data:
+        for ID, name in enumerate(data):
+            class_names[ID] = name.strip('\n')
+    # create dictionary to hold count of objects for image name
+    counts = dict()
+    # un-normalize bboxes
+    image = np.copy(img)
+    height, width, _ = image.shape
+    bboxes = bboxes * np.array([width, height, width, height, 1, 1])
 
-    def __init__(self, img_folder, num_images):
-        self.img_folder = img_folder
-        self.num_images = num_images
-        self.keyboard = Controller()
-
-        self.yolo = YOLOv4()
-        self.yolo.config.parse_names("modelData/coco.names")
-        self.yolo.config.parse_cfg("modelData/yolov4.cfg")
-        self.yolo.make_model()
-        self.yolo.load_weights("modelData/yolov4.weights", weights_type="yolo")
-        self.yolo.summary(summary_type="yolo")
-        self.yolo.summary()
-
-    def inferImages(self):
-        # These shenanigans were done to account for APRICOT's image filenames, to put back to normal just use img_id:
-        imgfile = ''
-        for i in range(self.num_images):
-            if i in range(0, 38):
-                imgfile = f'frs4_{i}'
-                if i == 4 or i == 9 or i == 32:
-                    imgfile = f'frs4_{i+1}'
-
-                elif i in range(38, 49):
-                    imgfile = f'frs10_{i - 38}'
-                    if i == 38 + 2:
-                        imgfile = f'frs10_{i - 38+1}'
-
-                elif i in range(49, 84):
-                    imgfile = f'rrc3_{i - 49}'
-
-                elif i in range(84, 97):
-                    imgfile = f'rrc6_{i - 84}'
-
-                elif i in range(97, 131):
-                    imgfile = f'sms2_{i - 97}'
-                    if i == 97 + 15 or i == 97 + 28:
-                        imgfile = f'sms2_{i - 97+1}'
-
-                elif i in range(131, 144):
-                    imgfile = f'sms8_{i - 131}'
-
-                # Retrieve image.
-                image_path = f'{self.img_folder}\\{imgfile}.jpg'
-                self.yolo.inference(media_path=image_path, cv_frame_size=(640, 480))
-                self.keyboard.press('q')
-                self.keyboard.release('q')
-
-
-CropBBox(img_folder='D:\\Coding\\PyCharmProjects\\ComputerSecurityProject\\APRICOT\\Images\\dev', num_images=144).inferImages()
+    for bbox in bboxes:
+        # get count of class for part of image name
+        class_index = int(bbox[4])
+        class_name = class_names[class_index]
+        counts[class_name] = counts.get(class_name, 0) + 1
+        # get box values
+        c_x = int(bbox[0])
+        c_y = int(bbox[1])
+        half_w = int(bbox[2] / 2)
+        half_h = int(bbox[3] / 2)
+        # calculate x and y min and max
+        xmin = c_x - half_w
+        if xmin < 5:
+            xmin = 5
+        ymin = c_y - half_h
+        if ymin < 5:
+            ymin = 5
+        xmax = c_x + half_w
+        ymax = c_y + half_h
+        # crop detection from image and take an additional 5 pixels around all edges
+        cropped_img = image[ymin - 5:ymax + 5, xmin - 5:xmax + 5]
+        # construct image name and join it to path for saving crop properly
+        img_name = class_name + '_' + str(counts[class_name]) + '.jpg'
+        img_path = os.path.join(path, img_name)
+        # save image
+        cv2.imwrite(img_path, cropped_img)

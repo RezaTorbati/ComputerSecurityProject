4bfe538f1de5f6eec84aa1f5375ef197f348a8cd
Zachary Singleton <za.sing227@gmail.com>
2021-11-29 17:02:00 -0600

    Better file iteration for InferImages and added PyCharm files to gitignore

--- InferImages.py
+++ InferImages.py
@@ -1,5 +1,6 @@
 # Note that for this class to work, inference() in yolov4/common/base_class.py
 # Must be modified by adding "return bboxes" at the end of the function.
+import glob
 
 import cv2
 from yolov4.tf import YOLOv4
@@ -8,12 +9,10 @@ import CropBBox
 
 class InferImages:
 
-    def __init__(self, img_folder, num_images):
-        self.img_folder = img_folder
-        self.num_images = num_images
+    def __init__(self):
 
         self.yolo = YOLOv4()
-        self.yolo.config.parse_names("modelData/coco.names")
+        self.yolo.config.parse_names("modelData/adversary.names")
         self.yolo.config.parse_cfg("modelData/yolov4.cfg")
         self.yolo.make_model()
         self.yolo.load_weights("modelData/yolov4.weights", weights_type="yolo")
@@ -22,46 +21,20 @@ class InferImages:
 
     def inferImages(self):
         # These shenanigans were done to account for APRICOT's image filenames in the "dev" folder
-        imgfile = ''
-        for i in range(self.num_images):
-            if i in range(0, 38):
-                imgfile = f'frs4_{i}'
-                if i == 4 or i == 9 or i == 32:
-                    imgfile = f'frs4_{i+1}'
-                    ++i
-
-            elif i in range(38, 49):
-                imgfile = f'frs10_{i - 38}'
-                if i == 38 + 2:
-                    imgfile = f'frs10_{i - 38+1}'
-                    ++i
-
-            elif i in range(49, 84):
-                imgfile = f'rrc3_{i - 49}'
-
-            elif i in range(84, 97):
-                imgfile = f'rrc6_{i - 84}'
-
-            elif i in range(97, 131):
-                imgfile = f'sms2_{i - 97}'
-                if i == 97 + 15 or i == 97 + 28:
-                    imgfile = f'sms2_{i - 97+1}'
-                    ++i
-
-            elif i in range(131, 144):
-                imgfile = f'sms8_{i - 131}'
-
+        filenames = glob.glob('D:\\Coding\\PyCharmProjects\\ComputerSecurityProject\\APRICOT\\Images\\test\\*.jpg')
+        filenames.sort()
+        for image_path in filenames:
             # Retrieve image and run inference
-            image_path = f'{self.img_folder}\\{imgfile}.jpg'
+            print(image_path)
             bboxes = self.yolo.inference(media_path=image_path)
             # Create a directory for the cropped images
-            original_image = cv2.imread(image_path)
-            crop_path = os.path.join(os.getcwd(), 'croppedImages', imgfile)
-            try:
-                os.mkdir(crop_path)
-            except FileExistsError:
-                pass
+            #original_image = cv2.imread(image_path)
+            #crop_path = os.path.join(os.getcwd(), 'croppedImages', imgfile)
+            #try:
+             #   os.mkdir(crop_path)
+            #except FileExistsError:
+             #   pass
             # Crop the original image into separate images that only include the bounding boxes
-            CropBBox.crop_objects(original_image, bboxes, crop_path)
+            #CropBBox.crop_objects(original_image, bboxes, crop_path)
 
-InferImages(img_folder='D:\\Coding\\PyCharmProjects\\ComputerSecurityProject\\APRICOT\\Images\\dev', num_images=144).inferImages()
+InferImages().inferImages()

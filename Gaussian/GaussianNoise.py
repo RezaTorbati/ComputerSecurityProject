4379e7ad2272d6e6671f3d508740bbd9c440d186
RezaTorbati <kofcrotmgiv@gmail.com>
2021-12-03 13:38:11 -0600

    Commiting everything related to Gaussian noise that is not too big to upload

---
+++ Gaussian/GaussianNoise.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+from PIL import Image
+from yolov4.tf import YOLOv4
+
+#Prints out the yolo detections of the image with and without noise
+def gaussianTest(yolo, imName, prob=.25, writeName='', showImage = True, var=.25, verbose = True):
+    image = np.asarray(Image.open(imName).convert("RGB"))
+
+    noNoiseResults = yolo.predict(image, prob) #gets initial yolo predictions
+
+    #Gets the noisy predictions
+    noisy = addNoise(image, var)
+    noisyResults = yolo.predict(noisy, prob)
+
+    label = imName.replace("images", "labels")
+    label = label.replace(".jpg", ".txt")
+    
+    noNoiseFN, noNoiseFP, truePositives, trueAdversaries = getResults(noNoiseResults, label)
+    noisyFN, noisyFP, truePositives, trueAdversaries = getResults(noisyResults, label)
+
+    if verbose:
+        print('True positives: ', truePositives)
+        print('True adversaries: ', trueAdversaries)
+        
+        print('No Noise False Negatives: ', noNoiseFN)
+        print('No Noise Adversaries Detected: ', noNoiseFP)
+        
+        print('Noisy False Negatives: ', noisyFN)
+        print('Noisy Adversaries Detected: ', noisyFP)
+
+    if writeName != '' or showImage:
+        #gets the bounding boxes around the images
+        image = yolo.draw_bboxes(image, noNoiseResults)
+        noisy = yolo.draw_bboxes(noisy, noisyResults)
+        
+        #Combines the images into one   
+        images = np.vstack((image.astype(np.uint8),noisy.astype(np.uint8)))
+    
+    #Writes if specified
+    if writeName != '':
+        Image.fromarray(images).save(writeName)
+        
+    #Displays the image if specified
+    if showImage:
+        Image.fromarray(images).show()
+        
+    return noNoiseFN, noNoiseFP, noisyFN, noisyFP, truePositives, trueAdversaries
+
+def getResults(yoloResults, label):
+    falseNegatives = 0
+    falsePositives = 0
+    truePositives = 0
+    trueAdversaries = 0
+    try:
+        labels = open(label, 'r')
+        
+        for line in labels:
+            line = line.split()
+            if int(line[0]) == 80:
+                trueAdversaries += 1
+            else:
+                truePositives += 1
+            
+            detected = False
+            for r in yoloResults:
+                if match(line, r):
+                    detected = True
+                    
+            if detected and int(line[0]) == 80:
+                falsePositives+=1
+            
+            if not detected and int(line[0]) != 80:
+                falseNegatives+=1
+        labels.close()
+    
+        return falseNegatives, falsePositives, truePositives, trueAdversaries
+    except:
+        return 0,0,0,0
+    
+#Checks to see if the given label and the given result match
+def match(label, result):
+    tolerance = .05
+    #print(label, " ", result)
+    if(float(label[1]) > result[0] - tolerance and float(label[1]) < result[0] + tolerance):
+            if(float(label[2]) > result[1] - tolerance and float(label[2]) < result[1] + tolerance):
+                    if(float(label[3]) > result[2] - tolerance and float(label[3]) < result[2] + tolerance):
+                        if(float(label[4]) > result[3] - tolerance and float(label[4]) < result[3] + tolerance):
+                            return True
+    return False
+   
+#Adds gaussian noise to an image and returns it   
+def addNoise(image, var, mean=0):
+    row, col, ch = image.shape
+    noise = np.random.normal(mean, var**.5, (row,col,ch))
+    noise = noise.reshape(row, col, ch)
+    noisy = image + noise
+    return noisy
---
+++ Gaussian/analyze.py
@@ -0,0 +1,72 @@
+import cv2
+from yolov4.tf import YOLOv4
+from GaussianNoise import gaussianTest
+import platform
+import numpy as np
+from PIL import Image
+import os
+
+yolo = YOLOv4()
+
+yolo.config.parse_names("ModelData/coco.names")
+yolo.config.parse_cfg("ModelData/yolov4.cfg")
+
+yolo.make_model()
+yolo.load_weights("ModelData/yolov4.weights", weights_type="yolo")
+yolo.summary(summary_type="yolo")
+yolo.summary()
+
+count = 0
+variances = [.1, .4, .45]
+writer = open('results.txt', 'w')
+files = os.listdir('images/')
+
+for v in variances:
+    totalAdversaries = 0
+    adversaries = 0
+    totalPositives = 0
+    positives = 0
+    totalNoNoiseFN = 0
+    noNoiseFN = 0
+    totalNoisyFN = 0
+    noisyFN = 0
+    totalNoNoiseAdversary = 0
+    noNoiseAdversary = 0
+    totalNoisyAdversary = 0
+    noisyAdversary = 0
+
+    print('Starting ', v)
+    count = 0
+    for f in files:
+        count+=1
+        im = 'images/' + f
+        noNoiseFN, noNoiseAdversary, noisyFN, noisyAdversary, positives, adversaries = gaussianTest(yolo, im, showImage = False, verbose = False, var=v, writeName='')
+        
+        totalAdversaries += adversaries
+        totalPositives += positives
+        totalNoNoiseFN += noNoiseFN
+        totalNoNoiseAdversary += noNoiseAdversary
+        totalNoisyFN += noisyFN
+        totalNoisyAdversary += noisyAdversary
+        
+        if count%100 == 0:
+            print("Finished image ", count)
+
+    print("Finished ", v)
+    print("Total true positives: ", totalPositives)
+    print("Total adversaries: ", totalAdversaries)
+    print("Total No Noise False Negatives: ", totalNoNoiseFN)
+    print("Total No Noise Adversaries Detected: ", totalNoNoiseAdversary)
+    print("Total Noisy False Negatives: ", totalNoisyFN)
+    print("Total Noisy Adversaries Detected: ", totalNoisyAdversary)
+    
+    writer.write("Results for " + str(v) + " variance on Gaussian Noise\n")
+    writer.write("Total true positives: "+ str(totalPositives)+"\n")
+    writer.write("Total adversaries: "+ str(totalAdversaries)+"\n")
+    writer.write("Total No Noise False Negatives: "+ str(totalNoNoiseFN)+"\n")
+    writer.write("Total No Noise Adversaries Detected: "+ str(totalNoNoiseAdversary)+"\n")
+    writer.write("Total Noisy False Negatives: "+ str(totalNoisyFN)+"\n")
+    writer.write("Total Noisy Adversaries Detected: "+ str(totalNoisyAdversary)+"\n\n")
+    
+writer.close()
+
